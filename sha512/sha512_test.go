ad85b945e80e38135977aa8c5fe8107a7d9049ec
Chris Aumann <me@chr4.org>
2014-08-04 17:39:34 +0200

    Remove linebreaks from saltstrings
    Also actually use a newline in newline teststring

--- sha512/sha512_test.go
+++ sha512/sha512_test.go
@@ -16,45 +16,37 @@ func TestNew(t *testing.T) {
 		{
 			[]byte("$6$saltstring"),
 			[]byte("Hello world!"),
-			"$6$saltstring$svn8UoSVapNtMuq1ukKS4tPQd8iKwSMHWjl/O817G3uBnIFNjn" +
-				"QJuesI68u4OTLiBFdcbYEdFCoEOfaS35inz1",
+			"$6$saltstring$svn8UoSVapNtMuq1ukKS4tPQd8iKwSMHWjl/O817G3uBnIFNjnQJuesI68u4OTLiBFdcbYEdFCoEOfaS35inz1",
 			5000,
 		},
 		{
 			[]byte("$6$rounds=10000$saltstringsaltstring"),
 			[]byte("Hello world!"),
-			"$6$rounds=10000$saltstringsaltst$OW1/O6BYHV6BcXZu8QVeXbDWra3Oeqh" +
-				"0sbHbbMCVNSnCM/UrjmM0Dp8vOuZeHBy/YTBmSK6H9qs/y3RnOaw5v.",
+			"$6$rounds=10000$saltstringsaltst$OW1/O6BYHV6BcXZu8QVeXbDWra3Oeqh0sbHbbMCVNSnCM/UrjmM0Dp8vOuZeHBy/YTBmSK6H9qs/y3RnOaw5v.",
 			10000,
 		},
 		{
 			[]byte("$6$rounds=5000$toolongsaltstring"),
 			[]byte("This is just a test"),
-			"$6$rounds=5000$toolongsaltstrin$lQ8jolhgVRVhY4b5pZKaysCLi0QBxGoN" +
-				"eKQzQ3glMhwllF7oGDZxUhx1yxdYcz/e1JSbq3y6JMxxl8audkUEm0",
+			"$6$rounds=5000$toolongsaltstrin$lQ8jolhgVRVhY4b5pZKaysCLi0QBxGoNeKQzQ3glMhwllF7oGDZxUhx1yxdYcz/e1JSbq3y6JMxxl8audkUEm0",
 			5000,
 		},
 		{
 			[]byte("$6$rounds=1400$anotherlongsaltstring"),
-			[]byte("a very much longer text to encrypt.  " +
-				"This one even stretches over more" +
-				"than one line."),
-			"$6$rounds=1400$anotherlongsalts$POfYwTEok97VWcjxIiSOjiykti.o/pQs" +
-				".wPvMxQ6Fm7I6IoYN3CmLs66x9t0oSwbtEW7o7UmJEiDwGqd8p4ur1",
+			[]byte("a very much longer text to encrypt. Kinda long.\nWith a linebreak."),
+			"$6$rounds=1400$anotherlongsalts$g.gUQRW7IS01Gjoxx45nbrcyyZCRRdMKVSVIaXSBnk6HigfcRLqrj6E3SM9lFBsGJp4klvm1ygxV998PGCDiV/",
 			1400,
 		},
 		{
 			[]byte("$6$rounds=77777$short"),
 			[]byte("we have a short salt string but not a short password"),
-			"$6$rounds=77777$short$WuQyW2YR.hBNpjjRhpYD/ifIw05xdfeEyQoMxIXbkv" +
-				"r0gge1a1x3yRULJ5CCaUeOxFmtlcGZelFl5CxtgfiAc0",
+			"$6$rounds=77777$short$WuQyW2YR.hBNpjjRhpYD/ifIw05xdfeEyQoMxIXbkvr0gge1a1x3yRULJ5CCaUeOxFmtlcGZelFl5CxtgfiAc0",
 			77777,
 		},
 		{
 			[]byte("$6$rounds=123456$asaltof16chars.."),
 			[]byte("a short string"),
-			"$6$rounds=123456$asaltof16chars..$BtCwjqMJGx5hrJhZywWvt0RLE8uZ4o" +
-				"PwcelCjmw2kSYu.Ec6ycULevoBK25fs2xXgMNrCzIMVcgEJAstJeonj1",
+			"$6$rounds=123456$asaltof16chars..$BtCwjqMJGx5hrJhZywWvt0RLE8uZ4oPwcelCjmw2kSYu.Ec6ycULevoBK25fs2xXgMNrCzIMVcgEJAstJeonj1",
 			123456,
 		},
 	}
