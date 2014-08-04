2cfdfb7895da4349b8651e2d04f5cc1ddc800147
Chris Aumann <me@chr4.org>
2014-08-04 17:44:01 +0200

    Add tests for salt.Parse()

--- salt/salt_test.go
+++ salt/salt_test.go
@@ -4,7 +4,10 @@
 
 package salt
 
-import "testing"
+import (
+	"bytes"
+	"testing"
+)
 
 func TestNew(t *testing.T) {
 	// Test default rounds
@@ -17,7 +20,50 @@ func TestNew(t *testing.T) {
 	for i := 1; i <= 20; i++ {
 		s = New(i, 7000+i)
 		if len(s) != len(MagicPrefix)+len("rounds=XXXX$")+i {
-			t.Errorf("Expected len %d, got len %d", i, len(s))
+			t.Errorf("Expected len %d, got %d", i, len(s))
+		}
+	}
+}
+
+func TestParse(t *testing.T) {
+	data := []struct {
+		rawsalt     []byte
+		salt        []byte
+		rounds      int
+		isRoundsDef bool
+	}{
+		{
+			[]byte("$6$saltstring"),
+			[]byte("saltstring"),
+			5000,
+			false,
+		},
+		{
+			[]byte("$6$rounds=10000$thisisasalt"),
+			[]byte("thisisasalt"),
+			10000,
+			true,
+		},
+		{
+			[]byte("$6$rounds=10000$atoolongsaltstring"),
+			[]byte("atoolongsaltstri"),
+			10000,
+			true,
+		},
+	}
+	for _, d := range data {
+		salt, rounds, isRoundsDef, err := Parse(d.rawsalt)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if bytes.Compare(salt, d.salt) != 0 {
+			t.Errorf("Expected salt %s, got %s\n", d.salt, salt)
+		}
+		if rounds != d.rounds {
+			t.Errorf("Expected rounds %d, got %d\n", d.rounds, rounds)
+		}
+		if isRoundsDef != d.isRoundsDef {
+			t.Errorf("Expected isRoundsDef %v, got %v\n", d.isRoundsDef, isRoundsDef)
 		}
 	}
 }
